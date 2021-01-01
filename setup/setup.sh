ef882070664af732fa5c6de655aa14558a421347
Steven Ihde <sihde@linkedin.com>
2021-01-01 15:35:28 -0800

    use --with-new-pkgs for upgrade

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,6 +8,6 @@ mkdir -p /usr/local/share/keyrings
 cp RPM-GPG-KEY-azulsystems /usr/local/share/keyrings
 
 apt-get update
-apt-get -y upgrade
+apt-get -y upgrade --with-new-pkgs
 apt-get -y install zulu8-jre-headless
 apt-get -y autoremove --purge
