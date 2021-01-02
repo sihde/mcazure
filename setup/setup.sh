4f6a266339c33dd47ef3c7f8846dc467b7e7dbff
Steven Ihde <sihde@linkedin.com>
2021-01-01 16:14:38 -0800

    add user, home dir

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,3 +11,11 @@ apt-get update
 apt-get -y upgrade --with-new-pkgs
 apt-get -y install zulu8-jre-headless
 apt-get -y autoremove --purge
+
+adduser --disabled-login --quiet --home /var/lib/minecraft --no-create-home minecraft --gecos ""
+mkdir /var/lib/minecraft
+
+# Add mount point for home directory
+cat >> /etc/fstab <<EOF
+UUID=e0698c68-30d0-482a-9615-a6278be757b4 /var/lib/minecraft ext4 defaults 0 2
+EOF
