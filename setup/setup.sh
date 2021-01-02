372d52da37384dddac12e01f18b642ac22988f08
Steven Ihde <sihde@linkedin.com>
2021-01-01 16:32:35 -0800

    fix

--- setup/setup.sh
+++ setup/setup.sh
@@ -9,13 +9,13 @@ cp RPM-GPG-KEY-azulsystems /usr/local/share/keyrings
 
 apt-get update
 apt-get -y upgrade --with-new-pkgs
-apt-get -y install zulu8-jre-headless
+apt-get -y install zulu8-jre-headless emacs-nox
 apt-get -y autoremove --purge
 
-adduser --disabled-login --quiet --home /var/lib/minecraft --no-create-home minecraft --gecos ""
-mkdir /var/lib/minecraft
+adduser --disabled-login --quiet --home /srv/minecraft --no-create-home minecraft --gecos ""
+mkdir /srv
 
 # Add mount point for home directory
 cat >> /etc/fstab <<EOF
-UUID=e0698c68-30d0-482a-9615-a6278be757b4 /var/lib/minecraft ext4 defaults 0 2
+UUID=e0698c68-30d0-482a-9615-a6278be757b4 /srv ext4 defaults 0 2
 EOF
