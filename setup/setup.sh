8f2cc4331ed5b35ef94f11b6f7834a7fe4c5c702
Steven Ihde <sihde@linkedin.com>
2021-01-01 17:40:43 -0800

    mount /srv

--- setup/setup.sh
+++ setup/setup.sh
@@ -9,7 +9,7 @@ cp RPM-GPG-KEY-azulsystems /usr/local/share/keyrings
 
 apt-get update
 apt-get -y upgrade --with-new-pkgs
-apt-get -y install zulu8-jre-headless emacs-nox
+apt-get -y install zulu8-jre-headless
 apt-get -y autoremove --purge
 
 adduser --disabled-login --quiet --home /srv/minecraft --no-create-home minecraft --gecos ""
@@ -19,3 +19,5 @@ mkdir -p /srv
 cat >> /etc/fstab <<EOF
 UUID=e0698c68-30d0-482a-9615-a6278be757b4 /srv ext4 defaults 0 2
 EOF
+
+mount /srv
