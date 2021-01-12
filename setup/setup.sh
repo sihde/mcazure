eb51da6e22025c0fcec61d3a0afa2f61833852f8
Steven Ihde <sihde@linkedin.com>
2021-01-11 20:19:38 -0800

    Add more authorized keys

--- setup/setup.sh
+++ setup/setup.sh
@@ -50,5 +50,16 @@ az keyvault secret download --vault-name hamachi-mc-vault --name host-key-ed2551
 chmod 0600 hamachi-mc_ed25519_key
 mv hamachi-mc_ed25519_key /etc/ssh/
 ssh-keygen -y -f /etc/ssh/hamachi-mc_ed25519_key > /etc/ssh/hamachi-mc_ed25519_key.pub
+
+# Patch adds HostKey directive for the new key
 patch /etc/ssh/sshd_config sshd_config.diff 
 dpkg-reconfigure -f noninteractive openssh-server
+
+cat >> /home/sihde/.ssh/authorized_keys <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6bvTJwY3DJT1c2ukmUJMJaLvCQUHpall3zQRjOb/2M sihde@sihde-mn3.linkedin.biz
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKutxzLvxoo8rrwBgw/JWkPLewTpEGZDNqpPif7EpGit sihde@toocoo
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqPM2vgeGCz30yywQpUKLBd2PW49v4gsZWdP8WdSEKE sihde@sihde-ld1.linkedin.biz
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkUe9+nsoJNMhPWNXX8qzL9dq6NvcNuK8PvT19rvFrW sihde@DESKTOP-PPRJK48
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLDrdUadTMo+pD0IgDOfDnkWMyamfbRkGcsXqsififK sihde@DESKTOP-IART6IV
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICI8eR8jWqi/fCbiXEWijGYbixK0ZdDMuEsBDUR9PgFT sihde@pixel5
+EOF
