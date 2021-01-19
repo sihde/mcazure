73b0012f6446c3433f28a26b665b692854c4ece2
Steven Ihde <sihde@linkedin.com>
2021-01-18 21:26:27 -0800

    Add role assignments, start work on SMB share mount

--- setup/setup.sh
+++ setup/setup.sh
@@ -28,7 +28,7 @@ echo sysstat sysstat/enable boolean true | debconf-set-selections
 # Install and upgrade packages
 apt-get update
 apt-get -y upgrade --with-new-pkgs
-apt-get -y install zulu8-jre-headless azure-cli sysstat
+apt-get -y install zulu8-jre-headless azure-cli sysstat cifs-utils
 apt-get -y autoremove --purge
 
 # Create user
@@ -40,9 +40,15 @@ cat >> /etc/fstab <<EOF
 UUID=e0698c68-30d0-482a-9615-a6278be757b4 /srv ext4 defaults 0 2
 EOF
 
+
 # Log in with user-assigned managed identity that has KeyVault access
-az login --allow-no-subscriptions --identity \
+az login --identity \
   -u '/subscriptions/32c8a58f-efa7-4fee-8245-180c4c11257b/resourceGroups/mc-storage/providers/Microsoft.ManagedIdentity/userAssignedIdentities/hamachi-mc-id'
+
+# Get storage account key
+# az storage account keys list -g mc-storage -n hamachifiles --query "[0].value"|perl -pe 's#"([a-zA-Z0-9/+=]+)"#password=$1#' > creds.txt
+# Mount SMB share in fstab
+# //myaccountname.file.core.windows.net/mystorageshare /mnt/mymountpoint cifs vers=3.0,username=mystorageaccount,cred=/path/to/cred,dir_mode=0777,file_mode=0777
 
 # download ssh host keys from KeyVault and configure ssh server to use them
 restore_umask=$(umask -p)
