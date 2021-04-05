531714e6a04d463395677a7a5f1b4f721ea80c0c
Steven Ihde <sihde@linkedin.com>
2021-04-04 17:28:52 -0700

    More nftables/wireguard

--- setup/setup-vpn.sh
+++ setup/setup-vpn.sh
@@ -12,7 +12,7 @@ echo sysstat sysstat/enable boolean true | debconf-set-selections
 # Install and upgrade packages
 apt-get update
 apt-get -y upgrade --with-new-pkgs
-apt-get -y install wireguard
+apt-get -y install wireguard nftables
 apt-get -y autoremove --purge
 
 cat >> /home/sihde/.ssh/authorized_keys <<EOF
@@ -23,3 +23,28 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkUe9+nsoJNMhPWNXX8qzL9dq6NvcNuK8PvT19rvFrW
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLDrdUadTMo+pD0IgDOfDnkWMyamfbRkGcsXqsififK sihde@DESKTOP-IART6IV
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICI8eR8jWqi/fCbiXEWijGYbixK0ZdDMuEsBDUR9PgFT sihde@pixel5
 EOF
+
+# WireGuard config
+mkdir -p /etc/wireguard
+umask 0077
+wg genkey > /etc/wireguard/wg0.key
+
+cat > /etc/wireguard/wg0.conf <<EOF
+# Remember IPv4 depends on nftables NAT configuration
+[Interface]
+Address = 192.168.96.1/24
+PrivateKey = $(cat /etc/wireguard/wg0.key)
+ListenPort = 51820
+# Fail if NAT rule not installed
+PreUp = nft list table ip %i | grep -qF 192.168.96.0/24
+
+# MacBook
+[Peer]
+PublicKey = ljaDpSqtvP6mlI5Bjw+3BHPsEohhHBQ4UOeKEV9XFiI=
+AllowedIPs = 192.168.96.2/32
+EOF
+
+systemctl enable wg-quick@wg0.service
+
+cp nftables.conf /etc/nftables.conf
+
